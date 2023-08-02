69fa4f64dd8b2992a81936f14ba350e662bd0575
ChetanNagane <chetan.nagane27@gmail.com>
2023-08-02 22:48:36 +0530

    still fixing

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,18 @@
-echo "Build Start"
-pip install -r requirements.txt
-pip install pysqlite3-binary
-python3.9 manage.py collectstatic --noinput --clear
-echo "Build End"
+#!/bin/bash
+
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Install the latest version of pip
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
